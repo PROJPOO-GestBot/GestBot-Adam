efe3dcfc05b21720d3afc4571a669e39e5699f87
AdamSifate <adam.sifate@cpnv.ch>
2022-11-25 09:11:30 +0100

    AddDoc

--- bot.py
+++ bot.py
@@ -8,25 +8,8 @@ bot = discord.Bot()
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
-@bot.slash_command(name = "hello", description = "Say hello to the bot")
-async def hello(ctx):
-    await ctx.send("sus!")
-
-@bot.slash_command(name = "test", description = "rwar")
-async def test(ctx):
-    await ctx.send("testFonctionnnelle")
-
 @bot.event
-async def on_message():
-    
-
-
-
-
-
-
-
-
-
+async def on_message(message):
+    print ("test")
 
 bot.run("MTA0MzA3ODk4MTA5MzY5NTUzOQ.GIEHVb.p_ktQhqcrRxa-ZcDYkbZtVs-_5V1s3tD1tcTes")
