fc2a7d569ec4ef51aa2ead78f4d97d9b78ef0ba4
AdamSifate <adam.sifate@cpnv.ch>
2022-12-02 11:47:32 +0100

    modificationCalculLVL

--- libs/Susers.py
+++ libs/Susers.py
@@ -2,18 +2,30 @@ class Susers():
     
     def __init__(self, bot):
         self.bot = bot
+        
+    def CalculLVL(lvl, xp):
+        if (xp >= lvl *10):
+            return True
+        return False
+    
+    def Level(userID):
+        NotImplemented
+        return #requete SQL contenent le niveau
     
     def AddXP(numberXP,userID):
         #requete SQL
         print ("test")
+        lvl = Susers.Level(userID)
+        xpTotal = NotImplemented #requete SQL pour optenire le nombre total de XP
+        if Susers.CalculLVL(lvl, xpTotal):
+            NotImplemented # requete SQL pour augmenter le niveau de 1
         return 
-    
-    def LevelXP(userID):
-        #requete SQL
-        return
         
     def RemoveXP(numberXP, userID):
         # requete SQL
         return
-    def __str__(self):
-            return "users"
+    
+   
+    
+
+    
