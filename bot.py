e95dc493e9f3acd595f78d34e79b1741100de769
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2022-12-15 22:19:11 +0100

    Review Sprint 01

--- bot.py
+++ bot.py
@@ -4,15 +4,14 @@ from libs.Susers import Susers
 
 bot = discord.Bot()
 
-
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')    
-
+        bot.load_extension(f'cogs.{filename[:-3]}')
 
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
+
 
 bot.run("MTA0MzA3ODk4MTA5MzY5NTUzOQ.GIEHVb.p_ktQhqcrRxa-ZcDYkbZtVs-_5V1s3tD1tcTes")
--- libs/Susers.py
+++ libs/Susers.py
@@ -1,30 +1,26 @@
 class Susers():
-    
+
     def __init__(self, bot):
         self.bot = bot
-        
-    def CalculLvl(lvl, xp):
-        if (xp >= lvl *10):
+
+    #calcul lvl return boolean value. Either change function name, or return type
+    def calcul_lvl(self, lvl, xp):
+        if xp >= lvl * 10:
             return True
         return False
-    
-    def Level(userID):
-        NotImplemented
-        return #requete SQL contenent le niveau
-    
-    def AddXP(numberXP,userID):
-        #requete SQL
-        lvl = Susers.Level(userID)
-        xpTotal = NotImplemented #requete SQL pour optenire le nombre total de XP
-        if Susers.CalculLvl(lvl, xpTotal):
-            NotImplemented # requete SQL pour augmenter le niveau de 1
-        return 
-        
-    def RemoveXP(numberXP, userID):
+
+    #function name must be an action. Level must be changed.
+    def level(self, user_id):
+        NotImplemented  # requete SQL contenent le niveau
+
+    def add_xp(self, number_xp, user_id):
+        # requete SQL
+        lvl = Susers.level(user_id)
+        xp_total = NotImplemented  # requete SQL pour optenire le nombre total de XP
+        if Susers.calcul_lvl(lvl, xp_total):
+            NotImplemented  # requete SQL pour augmenter le niveau de 1
+        return
+
+    def remove_xp(self, number_xp, user_id):
         # requete SQL
         return
-    
-   
-    
-
-    
