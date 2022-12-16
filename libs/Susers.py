6b91dba45817b8aab077535a2dcd095741c032f1
AdamSifate <adam.sifate@cpnv.ch>
2022-12-16 08:13:26 +0100

    Last commit

--- libs/Susers.py
+++ libs/Susers.py
@@ -1,5 +1,4 @@
 class Susers():
-    
     def __init__(self, bot):
         self.bot = bot
         
@@ -9,10 +8,12 @@ class Susers():
         return False
     
     def Level(userID):
+        print("niveau")
         NotImplemented
         return #requete SQL contenent le niveau
     
     def AddXP(numberXP,userID):
+        print("ajout XP")
         #requete SQL
         lvl = Susers.Level(userID)
         xpTotal = NotImplemented #requete SQL pour optenire le nombre total de XP
@@ -21,6 +22,7 @@ class Susers():
         return 
         
     def RemoveXP(numberXP, userID):
+        print ("supprimer XP")
         # requete SQL
         return
     
