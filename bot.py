40afa8a0192e794c9352921063aa09fe52ff87ad
AdamSifate <adam.sifate@cpnv.ch>
2022-12-23 11:58:23 +0100

    RemoveTokenInCode

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import discord
 import os
+from dotenv import load_dotenv
 from libs.Susers import Susers
+
+load_dotenv()
 
 bot = discord.Bot()
 
@@ -14,4 +17,4 @@ async def on_ready():
     print(f"{bot.user} is ready and online!")
 
 
-bot.run("MTA0MzA3ODk4MTA5MzY5NTUzOQ.GIEHVb.p_ktQhqcrRxa-ZcDYkbZtVs-_5V1s3tD1tcTes")
+bot.run(os.environ['BOT_TOKEN'])
